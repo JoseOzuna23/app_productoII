0cf6fc1ed84760cac36d6735fe580a8a607892ad
JoseOzuna23 <hose-23@hotmail.com>
2023-05-25 18:30:47 -0400

    modificacion productoss

--- app_producto/client/src/App.jsx
+++ app_producto/client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes >
-          <Route path='/nuevoproducto' element={<ProductosFormulario />} />
+          <Route path='/' element={<ProductosFormulario />} />
           <Route path='/listaproductos' element={<ListaProductos />} />
           <Route path='/detalleproducto/:id' element={<DetalleProductos/>} />
           <Route path='/editarproducto/:id' element={<EditarProducto/>} />
